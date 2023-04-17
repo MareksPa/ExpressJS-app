f20e724b133c0184e534b3b7f99dfdae90f00c47
Mareks <pavlovsmareks@gmail.com>
2023-04-17 20:03:20 +0300

    Adding environment route, for task 1.2 Using routes/environment instead of api/environment

---
+++ routes/environment.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET  */
+router.get("/", (req, res) => {
+  const format = (req.query.format || "html").toLowerCase();
+  const envVars = process.env;
+  const envArray = Object.entries(envVars);
+
+  switch (format) {
+    case "json":
+      // Render JSON format
+      res.type("json").send(JSON.stringify(envVars, null, 2));
+      break;
+    case "xml":
+      // Render XML format
+      var xml = envArray
+        .map(([key, value]) => `<${key}>${value}</${key}>`)
+        .join("");
+      res.type("xml").send(`<ul>${xml}</ul>`);
+      break;
+
+    case "html":
+      // Render HTML format
+      var html = envArray
+        .map(([key, value]) => `<li>${key}: ${value}</li>`)
+        .join("");
+      res.type("html").send(`<ul>${html}</ul>`);
+      break;
+    default:
+      console.log("Format type not found.");
+      res.sendStatus(404);
+  }
+});
+
+module.exports = router;
